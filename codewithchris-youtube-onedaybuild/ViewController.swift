eecdd3bc04fc8efd54d37eb8929361db2052c87d
Michael Tamsil <michaeltamsil@gmail.com>
2020-07-04 09:59:19 +0700

    initial xcode project creation

---
+++ codewithchris-youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codewithchris-youtube-onedaybuild
+//
+//  Created by michael tamsil on 03/07/20.
+//  Copyright © 2020 michael tamsil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
